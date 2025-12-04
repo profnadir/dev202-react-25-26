df289c1ef1b5d9a19ec160e0487017d0e5eff104
profnadir <hamzanadir.dev@gmail.com>
2025-12-04 13:08:44 +0100

    s4

--- 01-hello-react/src/App.js
+++ 01-hello-react/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  const langages = ["html","css","js","react"];
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="red">HI DEV202</p>
+      <p></p>
+{/*       <Div></Div>
+      <H1></H1> */}
+
+      <ul>
+        {
+          langages.map((l,i) => <li key={i}>{l} <button >edit</button></li>)
+        }
+      </ul>
     </div>
   );
 }
---
+++ s04-2/App.js
@@ -0,0 +1,12 @@
+console.log("hi dev201");
+
+var a = "hi";
+
+
+
+console.log(a);
+
+function test() {
+    return (<p>test</p>)
+}
+
